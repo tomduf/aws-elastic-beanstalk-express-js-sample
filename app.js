42ecb3e7604c3f135545e6bd1bee251cdd900f32
Thomas DUFOUR <tomduf70@gmail.com>
2021-06-13 10:50:58 +0200

    Pipeline complet

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Bonjour tout le monde ! Comment ça va ?'));
+app.get('/', (req, res) => res.send('Bonjour tout le monde ! Comment ça va ?<br>Cette fois, la modif se fait en local'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
