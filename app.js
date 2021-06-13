05849a4f8bb1647ee2f27ff54ac9de766dbf48e9
Thomas DUFOUR <tomduf70@gmail.com>
2021-06-13 12:57:02 +0200

    Balises HTML

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Bonjour tout le monde ! Comment ça va ?<br>Cette fois, la modif se fait en local'));
+app.get('/', (req, res) => res.send('<h1>Bonjour tout le monde !</h1><p>Comment ça va ?<br>Cette fois, la modif se fait en local</p>'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
